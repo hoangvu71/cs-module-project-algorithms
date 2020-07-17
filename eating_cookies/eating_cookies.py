572fa48f8947fa4b867aaddb1c42649d3a98b5ea
Devin <hoangong71@gmail.com>
2020-07-16 17:59:05 -0700

    Done Eating cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,13 +2,36 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
+
+def eating_cookies(n, cache = [0 for i in range(11)]):
     # Your code here
+    if n <= 1:
+        cache[1] = 1
+        return 1
+    elif n == 2:
+        cache[2] = 2
+        return 2
 
-    pass
+    if cache[n]:
+        return cache[n]
+    
+    way_3 = eating_cookies(n - 3, cache)
+    way_2 = eating_cookies(n - 2, cache)
+    way_1 = eating_cookies(n - 1, cache)
 
-if __name__ == "__main__":
-    # Use the main function here to test out your implementation
-    num_cookies = 5
+    result = way_3 + way_2 + way_1
+    cache[n] = result
+    return result
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+import time
+
+start = time.time()
+print(f'{eating_cookies(50, [0 for i in range(51)])}')
+print(f'\nResult calculated in {time.time()-start:.5f} seconds')
+print('\n--------------------------------\n')
+
+# if __name__ == "__main__":
+#     # Use the main function here to test out your implementation
+#     num_cookies = 5
+
+#     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
